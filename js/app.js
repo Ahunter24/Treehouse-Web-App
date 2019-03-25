6e07dc7213d4913153027da69f5aeff5ca128c3c
Ahunter24 <thomascrown18@gmail.com>
2019-03-24 21:40:06 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,137 +1,149 @@
-$("document").ready(function() {
+ $(document).ready(function () {
 
-	/* Close alert message when X is clicked. */
+     // ANIMATEDLY DISPLAY THE NOTIFICATION COUNTER.
+     $('#noti_Counter')
+         .css({
+             opacity: 0
+         })
+         // ADD DYNAMIC VALUE (YOU CAN EXTRACT DATA FROM DATABASE OR XML).
+         .css({
+             top: '25px'
+         })
+         .animate({
+             top: '25px',
+             opacity: 1
+         }, 500);
 
-	$("#close-button").click(function() {
-		$("#alert").fadeOut("slow");
-	});
+     $('#noti_Button').click(function () {
 
+         // TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
+         $('#notifications').fadeToggle('fast', 'linear', function () {
+             if ($('#notifications').is(':hidden')) {
+                 $('#noti_Button').css('background-color', '#');
+             } else $('#noti_Button').css('background-color', '#'); // CHANGE BACKGROUND COLOR OF THE BUTTON.
+         });
 
-	/* Notifications Drop Down List */
+         $('#noti_Counter').fadeOut('slow'); // HIDE THE COUNTER.
 
-	$('#notification-bell').click(function () {
-		// Show notifications drop-down
-		if ($('#notifications').css("visibility") == "hidden") {
-			$('#notifications').css("visibility", "visible").fadeIn();
-		}
+         return false;
+     });
 
-		else {
-			$('#notifications').fadeToggle('slow', 'linear');
-		}
+     // HIDE NOTIFICATIONS WHEN CLICKED ANYWHERE ON THE PAGE.
+     $(document).click(function () {
+         $('#notifications').hide();
 
-		$("#alert-dot").fadeOut("slow");
-		return false;
+         // CHECK IF NOTIFICATION COUNTER IS HIDDEN.
+         if ($('#noti_Counter').is(':hidden')) {
+             // CHANGE BACKGROUND COLOR OF THE BUTTON.
+             $('#noti_Button').css('background-color', '#');
+         }
+     });
 
-	});
-
-	// Hide notifications drop-down when anywhere on the page is clicked
-	$(document).click(function () {
-		$('#notifications').fadeOut("slow");
-	});
-
-	// Do nothing when notifications drop-down is clicked
-	$('#notifications').click(function () {
-		return false;
-	});
-
-
-	/* Send Message */
-	/* Use JS to allow you to submit the form and display a confirmation the message was sent. You won't actually submit the form, just simulate the action using JavaScript. */
-	/* Use JS to display error messages if user isn’t selected or message field is empty.*/
-
-	//when user clicks send
-	$('#send-button').click(function(e) {
-		//prevent default button click behavior
-		e.preventDefault();
-		$('#message-form').hide();
-
-		//if either field is empty, display error message
-		if ($('input#search').val().length === 0 || $('#textarea').val().length === 0) {
-			$('#message-user').append('<div id="error"><p>Please include both a user name and a message. Fields cannot be empty.</p><p><button id="new-message">Try Again</button><p></div>');
-
-			//when user clicks new message button
-			$('#new-message').click(function(){
-				// remove confirmation message and button
-				$('#confirmation').remove();
-				// remove error message
-				$('#error').remove();
-				// revert back to original message form
-				$('#message-form').show();
-			});
-		}
-
-		else if ($('input#search').val().length > 0) {
-			//replace html in that section with message and button
-			$('#message-user').append('<div id="confirmation"><p>Thanks! Your message has been sent.</p><p><button id="new-message">New Message</button><p></div>');
-
-			//when user clicks new message button
-				$('#new-message').click(function(){
-					// remove confirmation message and button
-					$('#confirmation').remove();
-					// remove error message
-					$('#error').remove();
-					// reset form fields
-					$('#message-form')[0].reset();
-					// revert back to original message form
-					$('#message-form').show();
-				});
-			}
-	});
-
-	/* Auto-Complete Member Search */
-
-	$('input[id="search"]').autoComplete({
-		minChars: 1,
-		source: function(term, suggest){
-			term = term.toLowerCase();
-			var choices = ['Bucky Bibbler', 'Abraham Lincoln', 'Sheri Berry', 'Joan Pennington', 'Hillary Clinton', 'Erik Estrada', 'Jacob Jacobson', 'Weird Al Yankovich', 'Mickey Mouse', 'John Boy', 'Babe Ruth', 'Dolly Parton', 'Scout Finch'];
-			var matches = [];
-			for (i=0; i<choices.length; i++)
-				if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
-			suggest(matches);
-		}
-	});
+     $('#notifications').click(function () {
+         return false; // DO NOTHING WHEN CONTAINER IS CLICKED.
+     });
+ });
 
 
-	/* Local Storage */
 
-// Store settings when save button is clicked
-$('#save-button').click(function(e){
-	e.preventDefault();
-	var timezone = $('#timezone').val();
-	localStorage.setItem('mytimezone', timezone);
 
-	var email = $('#email-switch').prop('checked');
-	localStorage.setItem('myemail', email);
 
-	var public = $('#public-switch').prop('checked');
-	localStorage.setItem('myprofile', public);
-});
+ var ctx = document.getElementById("myLineChart").getContext('2d');
+ var myLineChart = new Chart(ctx, {
+     type: 'line',
+     data: {
+         labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+         datasets: [{
+             label: 'traffic',
+             data: [550, 1050, 900, 1500, 2000, 1500, 1800, 1110, 1160, 2050, 2200],
+             backgroundColor: '#f1f1fa',
+             borderColor: '#7477bf',
+             borderWidth: 1
+                }]
+     },
+     options: {
 
-	//Show saved timezone on reload
-	$('#timezone').val(localStorage.getItem('mytimezone'));
 
-	//Show saved checkboxes on reload
-		if (localStorage.getItem('myemail') == "true") {
-			$('#email-switch').prop('checked', true);
-		} else {
-			$('#email-switch').prop('checked', false);
-		}
+         scales: {
+             yAxes: [{
+                 ticks: {
+                     beginAtZero: true
+                 }
+                        }]
+         }
+     }
+ });
+ var ctx = document.getElementById("myChart").getContext('2d');
+ var myChart = new Chart(ctx, {
+     type: 'bar',
+     data: {
+         labels: ["S", "M", "W", "T", "F", "S"],
+         datasets: [{
+             label: 'daily traffic',
+             data: [100, 40, 250, 150, 160, 100, 60],
+             backgroundColor: ['rgb(116, 119, 191)', 'rgb(116, 119, 191)', 'rgb(116, 119, 191)', 'rgb(116, 119, 191)',
+'rgb(116, 119, 191)', 'rgb(116, 119, 191)'
+                        ],
+             borderColor: ['rgb(0, 0, 0)', 'rgb(0, 0, 0)', 'rgb(0, 0, 0)', 'rgb(0, 0, 0)', 'rgb(0, 0, 0)',
+                                'rgb(0, 0, 0)'
+                        ],
+             borderWidth: 1
+                }]
+     },
+     options: {
 
-		if (localStorage.getItem('myprofile') == "true") {
-			$('#public-switch').prop('checked', true);
-		} else {
-			$('#public-switch').prop('checked', false);
-		}
 
-	//When cancel button is clicked, clear local storage
-	$('#cancel-button').click(function(e){
-		e.preventDefault();
-		localStorage.clear();
-		// Revert back to defaults
-		$('#timezone').val('a');
-		$('#email-switch').prop('checked', false);
-		$('#public-switch').prop('checked', false);
-	});
+         scales: {
+             yAxes: [{
+                 ticks: {
+                     beginAtZero: true
+                 }
+                        }]
+         }
+     }
+ });
 
-});
+ var ctx = document.getElementById('myDoughnutChart').getContext('2d');
+ var myDoughnutChart = new Chart(ctx, {
+     type: 'doughnut',
+     data: {
+         labels: ["Phones", "Tablets", "Desktop"],
+
+         datasets: [{
+             label: "",
+             data: [50, 20, 30],
+             backgroundColor: [
+                    "#7477bf",
+                    "#DCDCDC",
+                    "#90ee90",
+
+
+                ],
+             borderColor: [
+                    "#000",
+                    "#000",
+                    "#000",
+
+
+                ],
+             borderWidth: [1, 1, 1, 1, 1]
+
+                }]
+     },
+
+     options: {
+
+         responsive: true,
+
+     },
+     legend: {
+         display: true,
+         position: "bottom",
+         labels: {
+             fontColor: "#333",
+             fontSize: 16
+         },
+     }
+
+ });
+
